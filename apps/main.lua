b42c1d8466a7a59240e57bc969bbc10ae3e25c39
Eric <eric.nobody@gmail.com>
2018-01-01 00:41:38 -0500

    Comp 44 Many doors

--- apps/main.lua
+++ apps/main.lua
@@ -1,6 +1,6 @@
 
 local Bundle = require "lib.bundle"
-local Airlock = require 'airlock'
+local Manager = require 'door_manager'
 local Door = require 'door'
 
 local Program = {}
@@ -11,12 +11,36 @@ local cables = {
 }
 
 function Program.start()
-  VM.log("Running Airlock")
-  Airlock.start()
-  local entrance = Door.new(cables.entrance,8)
-  Door.newUI("monitor_190","ENTRY",entrance,123)
-  Door.newUI("monitor_191","HELIPAD",entrance)
 
+  local area_1 = Door.new(Bundle:new(CABLE_SIDE,colors.white))
+  local area_6 = Door.new(Bundle:new(CABLE_SIDE,colors.green))
+  local area_7 = Door.new(Bundle:new(CABLE_SIDE,colors.blue))
+  local area_8 = Door.new(Bundle:new(CABLE_SIDE,colors.cyan))
+  local office = Door.new(Bundle:new(CABLE_SIDE,colors.black),nil,Bundle:new(CABLE_SIDE,colors.orange))
+  local gym = Door.new(Bundle:new(CABLE_SIDE,colors.red),nil,Bundle:new(CABLE_SIDE,colors.magenta))
+  local storage = Door.new(Bundle:new(CABLE_SIDE,colors.purple),nil,Bundle:new(CABLE_SIDE,colors.lightBlue))
+  
+  local doors = {
+    Door.newUI("monitor_96","STORAGE"),
+    Door.newUI("monitor_87","MAIN102"),
+    Door.newUI("monitor_86","MAIN103"),
+    Door.newUI("monitor_84","AREA 3"),
+    Door.newUI("monitor_92","AREA 6"),
+    Door.newUI("monitor_95","AREA 7"),
+    Door.newUI("monitor_81"," LOBBY ",area_1),
+    Door.newUI("monitor_82","AREA 1",area_1),
+    Door.newUI("monitor_90","AREA 5",area_6),
+    Door.newUI("monitor_91","AREA 6",area_6),
+    Door.newUI("monitor_93","AREA 6",area_7),
+    Door.newUI("monitor_94","AREA 7",area_7),
+    Door.newUI("monitor_89","AREA 7",area_8),
+    Door.newUI("monitor_88","AREA 8",area_8),
+    Door.newUI("monitor_83","OFF.101",office,123),
+    Door.newUI("monitor_85","  GYM  ",gym),
+    Door.newUI("monitor_97","STORAGE",storage)
+  }
+
+  Manager.start(doors)
 end
 
 return Program
