b40616de3dffff283d970fc34adc4ad14707f629
Eric <eric.nobody@gmail.com>
2019-02-21 00:41:08 -0500

    Working Telehub including info panels

--- apps/main.lua
+++ apps/main.lua
@@ -6,19 +6,21 @@ local static_ui = require "static_ui"
 local elevator = require 'elevator'
 local Teleporter = require "telehub"
 local teleUI = require "telehub_ui"
+local teleInfo = require "telehub_info"
 ---
 -- The Telehub Computer
 
 local CABLE_SIDE = "left"
 
----------------
---Server & UI--
----------------
+local function teleArriveSound(x,y,z)
+  local telesound = "/playsound fdi:event.teleport_general_arrive @a[%d,%d,%d,20]"
+  exec(telesound,x,y,z)
+end
 
-
-------------
---  Main  --
-------------
+local function teleLeaveSound(x,y,z)
+  local telesound = "/playsound fdi:event.teleport_general @a[%d,%d,%d,20]"
+  exec(telesound,x,y,z)
+end
 
 local Program = {}
 
@@ -36,18 +38,51 @@ function Program.start()
   
   local teleDoorSecE = Door.new(Bundle:new(CABLE_SIDE,colors.white))
   Door.lock(teleDoorSecE)
---  Door.newUI("monitor_286","TeleHub",teleDoorSecE)
+
+  local teleDoorDorm = Door.new(Bundle:new(CABLE_SIDE,colors.lightBlue))
+  Door.lock(teleDoorDorm)
+  
+  local emitter6 = peripheral.wrap("emitter_6")
+  local x,y,z=18,46,0
+  emitter6.setSpace(x-1,y,z-1,x+1,y+2,z+1)
+
   local teleSecE = Teleporter.start(
-    function() exec("tpx @a[x=18,y=46,z=0,r=1] 4 26 45 0") end,
+    function() teleLeaveSound(x,y+3,z) end,
+    function() exec("tpx @a[x=%d,y=%d,z=%d,r=1] 4 26 45 0",x,y,z) end,
+    8,10,
     teleDoorSecE,
     Bundle:new(CABLE_SIDE,colors.magenta),
-    Bundle:new(CABLE_SIDE,colors.orange)
+    Bundle:new(CABLE_SIDE,colors.orange),
+    emitter6
+  )
+  
+  local emitter7 = peripheral.wrap("emitter_7")
+  local a,b,c=48,46,-16
+  emitter7.setSpace(a-1,b,c-1,a+1,b+2,c+1)
+  local teleDorm2 = Teleporter.start(
+    function() teleLeaveSound(a,b,c) end,
+    function() exec("tpx @a[x=%d,y=%d,z=%d,r=1] 4 39 45 -16",a,b,c) end,
+    8,10,
+    teleDoorDorm,
+    Bundle:new(CABLE_SIDE,colors.lime),
+    Bundle:new(CABLE_SIDE,colors.yellow),
+    emitter7
   )
   
   teleUI.start("monitor_281","SECT. E",teleSecE)
---  audio_player.start_link("monitor_36","INFO","playsound fdi:event.part1.psa_welcome @a 65 47 7 1",60)
-      
---  gen_server.start_link(Program,{comp},{})
+  teleUI.start("monitor_282","DORM. 2",teleDorm2)
+  teleUI.start("monitor_283","DORM. 3")
+  teleUI.start("monitor_284","DORM. 1")
+  teleUI.start("monitor_285","SECT. A")
+  teleUI.start("monitor_286","SECT. D")
+  teleUI.start("monitor_287","SECT. C")
+  teleUI.start("monitor_288","SECT. B")
+  
+  teleInfo.start("monitor_297",{{"DORM.1","Offline"},{"SECT.A","Offline"},{"SECT.D","Offline"},{"SECT.E","Online"}})
+  teleInfo.start("monitor_296",{{"DORM.1","Offline"},{"SECT.A","Offline"},{"SECT.D","Offline"},{"SECT.E","Online"}})
+  teleInfo.start("monitor_298",{{"DORM.2","Online"},{"DORM.3","Offline"},{"SECT.B","Offline"},{"SECT.C","Offline"}})
+  teleInfo.start("monitor_299",{{"DORM.2","Online"},{"DORM.3","Offline"},{"SECT.B","Offline"},{"SECT.C","Offline"}})
+
 end
 
 return Program
